7e1db5179cc54f3b4a021549e6d948efb40dc00a
Ian Lyttle <ian.lyttle@schneider-electric.com>
2016-12-12 18:28:55 -0600

    adds utils

---
+++ R/utils.R
@@ -0,0 +1,27 @@
+#' validate that this is a valid endpoint
+#'
+#' @param endpoint    \code{AzureML::\link[AzureML]{endpoints}} (single member)
+#'
+#' @return \code{endpoint}
+#'
+validate_endpoint <- function(endpoint){
+
+  # taken from https://github.com/RevolutionAnalytics/AzureML/blob/master/R/consume.R
+  #
+  # I would rather ask the AzureML maintainers if they would consider a
+  # function like this one, then I could use theirs.
+  #
+  if(is.Service(endpoint))
+  {
+    if(nrow(endpoint) > 1) endpoint = endpoint[1, ]
+    default <- endpoint$DefaultEndpointName
+    endpoint <- endpoints(attr(endpoint, "workspace"), endpoint)
+    endpoint <- subset(endpoint, Name = default)
+  }
+
+  if(!is.Endpoint(endpoint)) {
+    stop("Invalid endpoint. Use publishWebservice() or endpoints() to create or obtain a service endpoint.")
+  }
+
+  endpoint
+}
